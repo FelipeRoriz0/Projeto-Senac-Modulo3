7e9b6243776e6639485b7e43c6ddee92efa5c8f0
Felipe Roriz <f.roriz@hotmail.com>
2022-06-22 12:13:15 -0300

    script do banco de dados

---
+++ Projeto_Java_Senac/scripts/CREATE_DATABASE_PROJETO.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE db_projeto;
+
+USE db_projeto;
+
+CREATE TABLE tb_respostas(
+id INT PRIMARY KEY AUTO_INCREMENT,
+resposta VARCHAR(80)
+);
+
+INSERT INTO tb_respostas (resposta) VALUES ('Rio de Janeiro');
+INSERT INTO tb_respostas (resposta) VALUES ('Kim Jong-un') ;
+INSERT INTO tb_respostas (resposta) VALUES ('Central park') ;
+INSERT INTO tb_respostas (resposta) VALUES ('Telescopio James Webb') ;
+INSERT INTO tb_respostas (resposta) VALUES ('Burj khalifa');
+INSERT INTO tb_respostas (resposta) VALUES ('Handpan');
